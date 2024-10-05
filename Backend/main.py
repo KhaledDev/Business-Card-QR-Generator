ef0c6533b2983ab663407f1f7933d13359c16c44
KhaledDev <56503740+KhaledDev@users.noreply.github.com>
2024-10-06 02:52:14 +0400

    Updated main.py
    started using flask for the backend
    created a small test only
    and added requirements.txt for later on

--- Backend/main.py
+++ Backend/main.py
@@ -1,1 +1,16 @@
-print("hello world!")
+from flask import Flask
+
+app = Flask("Test")
+
+@app.route("/upload_img", methods=["POST"])
+def upload_img():
+    # Connect to the cloudflare server and upload the image
+    pass
+
+@app.route("/get_img", methods=["GET"])
+def get_img():
+    # Connect to the cloudflare server and get image link.
+    pass
+
+# run server
+app.run(port=5000, debug=True)
