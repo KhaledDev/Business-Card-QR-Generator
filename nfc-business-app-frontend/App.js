6842cb7561b73097f1d007e9b00a53a664e57e58
KhaledDev <56503740+KhaledDev@users.noreply.github.com>
2024-10-28 20:12:33 +0400

    Created the frontend brach, will start working on it
    we love react native!!

---
+++ nfc-business-app-frontend/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Test from './components/test';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>copium</Text>
+      <StatusBar style="auto" />
+      <Test />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
