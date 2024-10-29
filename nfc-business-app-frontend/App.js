c2e52fe41bcbe5edf01556ae1d07ac3cc8cd50b0
KhaledDev <56503740+KhaledDev@users.noreply.github.com>
2024-10-29 11:43:36 +0400

    Started on a small ui.
    TODO: Resize the image, add the nfc capability

--- nfc-business-app-frontend/App.js
+++ nfc-business-app-frontend/App.js
@@ -1,13 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Test from './components/test';
+import { StyleSheet, Text, TouchableOpacity,Image, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>copium</Text>
-      <StatusBar style="auto" />
-      <Test />
+      <Text style={styles.text}t>Scan your NFC card</Text>
+      <StatusBar style="auto"/>
+      
+      <Image styles={styles.image} source={require('./assets/contactless.png')} />
+      
+      <TouchableOpacity style={styles.import_btn}>
+        <Text>Import card</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -16,7 +20,23 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    marginTop: 50,
     alignItems: 'center',
-    justifyContent: 'center',
   },
+  text:{
+    fontSize: 25,
+    fontWeight: 'bold',
+  },
+  image: {
+    width: 25,
+    height: 25,
+    resizeMode: "stretch",
+  },
+  import_btn: {
+    backgroundColor: '#bde0fe',
+    padding: 10,
+    borderRadius: 10,
+    alignItems: 'center',
+    marginTop: 50,
+  }
 });
